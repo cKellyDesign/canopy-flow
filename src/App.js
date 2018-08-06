32a2d725ac1f1ef0c66f756d142baaa785161277
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-08-06 14:28:10 +0300

    Test out API basic auth
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Button, HelpBlock } from 'react-bootstrap';
 import './App.css';
+import { loginUser, logoutUser } from './store/actions';
 
 function FieldGroup({ id, label, help, ...props }) {
   return (
@@ -12,29 +14,74 @@ function FieldGroup({ id, label, help, ...props }) {
   );
 }
 
+const mapStateToProps = (state, ownProps) => {
+  console.log("sttae", state)
+  console.log("own props", ownProps);
+  return {
+    global: state,
+  }
+}
+
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};  
+  }
+
+  handleClickEvent(e) {
+    e.preventDefault();
+    const username = this.refs.username;
+    const password = this.refs.password;
+    const creds = { username: username.value.trim(), password: password.value.trim() }
+    this.props.onLoginClick(creds)
+  }
+
   render() {
     return (
       <div>
         <form>
-          <FieldGroup
+          <input
             id="username"
+            ref="username"
             type="text"
             label="Username"
             placeholder="Enter Username"
           />
-          <FieldGroup
+          <input
             id="password"
+            ref="password"
             type="password"
             label="Password"
             placeholder="Enter Password"
           />
-          <Button bsStyle="primary" bsSize="lg" type="submit">Sign In</Button>
+          <Button
+          bsStyle="primary"
+          bsSize="lg"
+          type="submit"
+          onClick={(e) => this.handleClickEvent(e)}>Sign In</Button>
+          {this.props && this.props.errorMessage ? 
+          <p>{this.props.errorMessage}</p>
+        : null}
         </form>
       </div>
     );
   }
 };
+
+class Logout extends Component {
+  render() {
+    const { onLogoutClick } = this.props;
+    return (
+      <div>
+       <Button
+          bsStyle="primary"
+          bsSize="lg"
+          onClick={() => onLogoutClick()}
+          >Sign Out</Button>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -44,41 +91,50 @@ class App extends Component {
       views: {
         'login': {},
       },
-      isLoggedIn: false,      
+      isLoggedIn: false,
     }
   }
   render() {
+    console.log(this.props)
     return (
       <div className="App">
-        {!this.state.isLoggedIn ? (
+        {!this.props.global.isAuthenticated ? (
           <Grid>
             <Row>
               <Col sm={12} md={4} mdOffset={4}>
                 <div className="login">
                   <h1>Log In</h1>
-                  <Login />
+                  <Login
+                    onLoginClick={creds => this.props.dispatch(loginUser(creds))}
+                    errorMessage={this.props.global.errorMessage}
+                  />
                 </div>
               </Col>
             </Row>
           </Grid>
         ) : (
-          <Grid>
-            <Row className="header">
-              <Col sm={12}><h1 className="title">Canopy Flow</h1></Col>
-            </Row>
-            <Row className="main">
-              <Col sm={6} md={3}>
-                <h3>Flows</h3>
-              </Col>
-              <Col sm={6} md={9}>
-                <h3>Flow Details</h3>
-              </Col>
-            </Row>
-          </Grid>
-        )}
+            <Grid>
+              <Row className="header">
+                <Col sm={12}><h1 className="title">Canopy Flow</h1></Col>
+              </Row>
+              <Row className="main">
+                <Col sm={6} md={3}>
+                  <h3>Flows</h3>
+                </Col>
+                <Col sm={6} md={9}>
+                  <h3>Flow Details</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Logout
+                  onLogoutClick={() => this.props.dispatch(logoutUser())}
+                />
+              </Row>
+            </Grid>
+          )}
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/store/reducers.js
@@ -0,0 +1,46 @@
+import { LOGIN_SUCCESS, LOGIN_REQUEST, LOGIN_FAILURE, LOGOUT_SUCCESS } from './actions';
+
+export default function AUTH(state = {
+  isFetching: false,
+  isAuthenticated: localStorage.getItem('success') ? true : false
+}, action) {
+  switch (action.type) {
+    case LOGIN_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+        isAuthenticated: false,
+        user: action.creds
+      };
+    }
+
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        isAuthenticated: true,
+        errorMessage: ''
+      };
+    }
+
+    case LOGIN_FAILURE: {
+      return {
+        ...state,
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: action.message,
+      };
+    }
+
+  case LOGOUT_SUCCESS: {
+    return {
+      ...state,
+      isFetching: true,
+      isAuthenticated: false,
+    }
+  }
+
+    default:
+      return state;
+  }
+}
